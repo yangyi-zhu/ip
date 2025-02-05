37ba24ebe5df940a32ce607105296d54a7e5e811
yangyi-zhu <142135488+yangyi-zhu@users.noreply.github.com>
2025-02-06 03:31:12 +0800

    Implemented ability to store user input and display list upon request

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,12 @@
 import java.util.Scanner;
 
 public class Main {
+    public static final String DIVIDER = "--------------------------";
+
+    private static int count = 0;
+    private static final String[] list = new String[100];
+
+    /**
     public static void echo(String msg) {
         if (msg.equals("bye")) return;
         System.out.println("\n" + "-------------------------- \n" +
@@ -9,17 +15,42 @@ public class Main {
         Scanner newMsg = new Scanner(System.in);
         echo(newMsg.nextLine());
     }
+     */
+
+    public static void checkInput(String msg) {
+        switch (msg) {
+        case "list":
+            System.out.println("\n" + DIVIDER);
+            for (int i = 0; i < count; i++) {
+                System.out.println((i + 1) + ". " + list[i]);
+            }
+            System.out.println(DIVIDER + "\n");
+            break;
+        default:
+            list[count] = msg;
+            count++;
+            System.out.println("\n" + DIVIDER + "\n" +
+                    "added: " + msg + "\n" +
+                    DIVIDER + "\n");
+        }
+    }
 
     public static void main(String[] args) {
         Scanner msg = new Scanner(System.in);
-        System.out.println("-------------------------- \n" +
+        System.out.println(DIVIDER + "\n" +
                 "Bonjour et bievenue! Vous pouvez m'appeler Baguette! \n" +
                 "What do you think about my intro? :D \n" +
                 "Anyhow, what are we thinking today? \n" +
-                "-------------------------- \n");
-        echo(msg.nextLine());
-        System.out.println("\n" + "-------------------------- \n" +
-                "Have a nice day! \n" +
-                "--------------------------");
+                DIVIDER + "\n");
+        while (true) {
+            String input = msg.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("\n" + DIVIDER + "\n" +
+                        "Have a nice day! \n" +
+                        DIVIDER);
+                return;
+            }
+            checkInput(input);
+        }
     }
 }
